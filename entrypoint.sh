72a4ff21c6ea2938b808dfd292046664e3aab616
decinho13 <47136849+decinho13@users.noreply.github.com>
2019-03-27 16:20:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,27 +4,13 @@ set -ex
 # Source ROS setuo
 source  /opt/ros/kinetic/setup.bash
 
-# Source enviroment variables for Gazebo
 
-RUN_FLUXBOX=${RUN_FLUXBOX:-yes}
-RUN_XTERM=${RUN_XTERM:-yes}
-
-case $RUN_FLUXBOX in
-  false|no|n|0)
-    rm -f /app/conf.d/fluxbox.conf
-    ;;
-esac
-
-case $RUN_XTERM in
-  false|no|n|0)
-    rm -f /app/conf.d/xterm.conf
-    ;;
-esac
+exec supervisord -c /app/supervisord.conf &
 
 cd ~/c9sdk
-node server.js --listen 0.0.0.0 --port 8181 -w /workspace/src &
+node server.js --listen 0.0.0.0 --port 8181 -w /workspace/src
 
-exec supervisord -c /app/supervisord.conf 
+
 
 
 
