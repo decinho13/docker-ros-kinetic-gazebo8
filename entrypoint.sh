243a78f140bf90f20a273781345346f8c27b2de7
decinho13 <47136849+decinho13@users.noreply.github.com>
2019-03-28 12:50:01 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,9 @@ source  /opt/ros/kinetic/setup.bash
 exec supervisord -c /app/supervisord.conf &
 
 cd ~/c9sdk
-node server.js --listen 0.0.0.0 --port 8181 -w /workspace/src
+node server.js --listen 0.0.0.0 --port 8181 -w /workspace/src &
+
+./root/gzweb/start_gzweb.sh && gzserver
 
 
 
