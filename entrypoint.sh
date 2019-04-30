19195df9b37ef369776f84201a68b7824c9555b9
decinho13 <47136849+decinho13@users.noreply.github.com>
2019-04-30 14:24:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -ex
 source  /opt/ros/kinetic/setup.bash
 
 
-exec supervisord -c /home/supervisord.conf &
+exec supervisord -c /app/supervisord.conf &
 
 #cd /home/c9sdk
 #node server.js --listen 0.0.0.0 --port 8181 -w /
