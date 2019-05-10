eb2dc128663dfd46f2a3b3b3ad14d8628d890193
decinho13 <47136849+decinho13@users.noreply.github.com>
2019-05-10 11:30:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ source  /opt/ros/kinetic/setup.bash
 
 exec supervisord -c /opt/app-root/bin/supervisord.conf &
 
-cd /root/c9sdk
+cd /opt/app-root/bin/c9sdk
 npm install pty.js
 node server.js --listen 0.0.0.0 --port 8181 -w /
