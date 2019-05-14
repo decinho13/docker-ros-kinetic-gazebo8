083979c71dd97ce65372b6bee58a4057fb2f76c2
decinho13 <47136849+decinho13@users.noreply.github.com>
2019-05-14 08:52:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ exec supervisord -c /opt/app-root/bin/supervisord.conf &
 
 cd /opt/app-root/bin/c9sdk
 npm install pty.js
-node server.js --listen 0.0.0.0 --port 8181 -w /
+node server.js --listen 0.0.0.0 --port 8181 -w /opt/app-root/bin
